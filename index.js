ff50de4dc5c0cec51d9b5a997375c9acd81d1374
Tdogb <tdogbeard@gmail.com>
2019-02-04 12:16:09 -0500

    1

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-var secret = JE2t1E6rcs6TANoduwDxvSnsNBl7aui98dmshc2f
+var config = {
+		apiKey: "AIzaSyDs-eaEU022QCGXE2Owc1mWfnRdeAtj4dI",
+		authDomain: "scouting-system-b009f.firebaseapp.com",
+		databaseURL: "https://scouting-system-b009f.firebaseio.com",
+		projectId: "scouting-system-b009f",
+		storageBucket: "scouting-system-b009f.appspot.com",
+		messagingSenderId: "140177617153"
+		};
+firebase.initializeApp(config);
 
+var usersRef = firebase.database().ref("users/");
+
+userRef.set({
+	Tigey: {
+		number: 10
+	},
+	Tanner: {
+		number: 2
+	}
+});
+
+console.log(firebase.database().ref('users'));
